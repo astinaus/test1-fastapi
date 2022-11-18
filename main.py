149fc5ac5fe1464319f05b2bf0e35d7e4506586c
astinaus <slaayers0@gmail.com>
2022-11-18 14:29:02 +0900

    에러처리

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+from fastapi.encoders import jsonable_encoder
 from pydantic import BaseModel
+from fastapi.responses import JSONResponse
+
+class ResponseDTO(BaseModel):
+    code: int
+    message: str
+    data: object
 
 
 class Cat(BaseModel):
@@ -23,3 +30,17 @@ async def second(skip: int = 0, limit: int = 10):
 @app.post("/cat")
 async def cat(cat: Cat):
     return cat
+
+
+@app.get("/error")
+async def error():
+    dto = ResponseDTO(
+        code=1,
+        message="페이지가 없습니다.",
+        data=None
+    )
+    return JSONResponse(status_code=404, content=jsonable_encoder(dto))
+
+@app.get("/error1")
+async def error1():
+    raise HTTPException(status_code=404, detail={"message": "Item not found"})
